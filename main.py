392c2789ba8836d90ed7f04a5b78851809676b8e
junho-baek <86907815+junho-baek@users.noreply.github.com>
2023-12-09 14:26:13 +0000

    #3.1 Predict Messages (05:04)
    스트링을 예측하는 것이 아니라 메세지리스트를 입력받아 메세지를 예측(대답)하는 모델을 설정했다. 하지만 이 모델에도 부족한 점이 있는데 그것은 바로 하드코딩, 매직넘버를 사용했다는 사실! 변수를 사용할 수 있도록 수정해보자.. 다음 강의에서 말이지.

--- main.py
+++ main.py
@@ -2,10 +2,18 @@ from langchain.chat_models import ChatOpenAI
 
 
 
-chat = ChatOpenAI(temperature=0.1)
+chat = ChatOpenAI(
+  temperature=0.1 #모델의 창의성, 높을 수록 창의적이다
+)
 
+from langchain.schema import HumanMessage, AIMessage, SystemMessage, messages
 
-b = chat.predict("안녕?")
+messages = [
+  SystemMessage(content="You are a geography expert. And you only reply in korean"),
+  AIMessage(content="안녕 내이름은 색마야"),
+  HumanMessage(content="야 서울 부산 거리가 어떻게 돼? 그리고 너 이름이 뭐야?")
+]
 
+result= chat.predict_messages(messages)
 
-print(b)
+print(result)
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 ### .env 는 replit 에서 안돌아가서 해당 서비스에서 제공하는 secrets를 사용함
+## source ~/.bashrc 
+## venv, run 이용 가능
