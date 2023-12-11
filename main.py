616136f37b0bf97d66504e23faee1dc554fd2a9b
junho-baek <86907815+junho-baek@users.noreply.github.com>
2023-12-11 14:18:31 +0000

    #3.2 Prompt Templates (07:59)
    PromptTemplate 와 ChatPromptTemplate 를 사용하는 법을 배웠다.
    ChatPromptTemplate.from_messages라는 메소드는 리스트를 인자로 받고, 리스트 요소는 길이가 2인 튜플 3개를 받는다. 템플릿는 format_messages 라는 메소드로 각각 인자를 받을 수 있다.

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 from langchain.chat_models import ChatOpenAI
+from langchain.prompts import PromptTemplate, ChatPromptTemplate
+
+chat = ChatOpenAI(temperature=0.1)
+
+# template = PromptTemplate.from_template("What is the distance between {country_a} and {country_b}?")
+
+# prompt = template.format(country_a="South Korea", country_b="Japan")
+
+# print(chat.predict(prompt))
 
 
+template = ChatPromptTemplate.from_messages(
+  [
+  ("system", "You are a geography expert. And you only reply in {language}"),
+  ("ai", "Hi, my name is {name}"),
+  ("human", "What is the distance between {country_a} and {country_b}?. Also, what is your name?")
 
-chat = ChatOpenAI(
-  temperature=0.1 #모델의 창의성, 높을 수록 창의적이다
+  ]
 )
 
-from langchain.schema import HumanMessage, AIMessage, SystemMessage, messages
+prompt = template.format_messages(
+  language="korean",
+  name="탱이",
+  country_a="South Korea",
+  country_b="Japan"
+)
 
-messages = [
-  SystemMessage(content="You are a geography expert. And you only reply in korean"),
-  AIMessage(content="안녕 내이름은 색마야"),
-  HumanMessage(content="야 서울 부산 거리가 어떻게 돼? 그리고 너 이름이 뭐야?")
-]
-
-result= chat.predict_messages(messages)
-
-print(result)
+print(chat.predict_messages(prompt))
 
 
--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 ### .env 는 replit 에서 안돌아가서 해당 서비스에서 제공하는 secrets를 사용함
 ## source ~/.bashrc 
 ## venv, run 이용 가능
+## alias
+## alias venv='source env/bin/activate'
+## alias run='python main.py'
