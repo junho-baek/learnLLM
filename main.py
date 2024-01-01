22f94ab252aa86f7cc2315fe7d8fa57d075b3994
junho-baek <86907815+junho-baek@users.noreply.github.com>
2024-01-01 14:09:01 +0000

    #4.4 Serialization and Composition (08:12)-1
    json, yaml에 템플릿을 저장하고 from langchain.prompts import load_prompt 라이브러리로 불러와서 사용할 수 있는 코드

--- main.py
+++ main.py
@@ -1,80 +1,15 @@
 from langchain.chat_models import ChatOpenAI
-from langchain.prompts import example_selector
-from langchain.prompts.few_shot import FewShotPromptTemplate
 from langchain.callbacks import StreamingStdOutCallbackHandler
-from langchain.prompts.prompt import PromptTemplate
-from langchain.prompts.example_selector.base import BaseExampleSelector
-from langchain.prompts.example_selector import LengthBasedExampleSelector  # 길이기반으로 예제들을 선택해준다.
+from langchain.prompts import load_prompt #yaml, json에 prompt 템플릿을 저장하고 쓸 수 있다.
+from langchain.prompts import PromptTemplate
+from langchain.prompts.pipeline import PipelinePromptTemplate #많은 프롬프트들을 합칠 수 있는 라이브러리
+
+prompt = load_prompt("./prompt.yaml")
+# prompt = load_prompt("./prompt.json")
+
 
 chat = ChatOpenAI(temperature=0.1,
                   streaming=True,
                   callbacks=[StreamingStdOutCallbackHandler()])
 
-examples = [
-    {
-        "question":
-        "What do you know about France?",
-        "answer":
-        """
-Here is what I know:
-Capital: Paris
-Language: French
-Food: Wine and Cheese
-Currency: Euro
-""",
-    },
-    {
-        "question":
-        "What do you know about Italy?",
-        "answer":
-        """
-I know this:
-Capital: Rome
-Language: Italian
-Food: Pizza and Pasta
-Currency: Euro
-""",
-    },
-    {
-        "question":
-        "What do you know about Greece?",
-        "answer":
-        """
-I know this:
-Capital: Athens
-Language: Greek
-Food: Souvlaki and Feta Cheese
-Currency: Euro
-""",
-    },
-]
-#exampleSelector는 아래와 같은 메서드를 가지고 있어야 합니다. add, select_examples
-class RandomExampleSelector(BaseExampleSelector):
-  def __init__(self, examples):
-      self.examples = examples
-
-  def add_example(self, example):
-      self.examples.append(example)
-
-  def select_examples(self, input_variables):
-      from random import choice
-
-      return [choice(self.examples)]
-
-
-example_prompt = PromptTemplate.from_template("Human: {question}\nAI:{answer}")
-
-example_selector = RandomExampleSelector(
-    examples=examples,
-)
-# 개체가 생성될 때 이미 예제들을 넣어둠
-print(example_selector)
-prompt = FewShotPromptTemplate(
-    example_prompt=example_prompt,
-    example_selector=example_selector, # 개체를 받는 파라미터
-    suffix="Human: What do you know about {country}?",
-    input_variables=["country"],
-)
-
-
-print(prompt.format(country="Brazil"))
+print(prompt.format(country="xxx"))
