e9fc834ba27fcebc76b9309d94eedfd6f0dad41e
junho-baek <86907815+junho-baek@users.noreply.github.com>
2024-01-02 14:10:25 +0000

    #4.5 Caching (05:54)
    캐싱을 쓰면 메모리에 저장된 이미 대답했던 것, 혹은 데이터베이스에 있는 것, 그 외 수많은 것들에서 이미 대답이 되어있는 것에 AI자원을 아껴서 사용할 수 있다. 디버그모드 또한 배웠다.

--- main.py
+++ main.py
@@ -1,56 +1,19 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.callbacks import StreamingStdOutCallbackHandler
-from langchain.prompts import PromptTemplate
-from langchain.prompts.pipeline import PipelinePromptTemplate  #많은 프롬프트들을 합칠 수 있는 라이브러리
+from langchain.globals import set_llm_cache, set_debug
+from langchain.cache import InMemoryCache, SQLiteCache
+
+set_llm_cache(SQLiteCache("cache.db"))
 
 
-chat = ChatOpenAI(temperature=0.1,
-                  streaming=True,
-                  callbacks=[StreamingStdOutCallbackHandler()])
-
-intro = PromptTemplate.from_template("""
-    You are a role playing assistant.
-    And you are impersonating a {character}
-""")
-
-example = PromptTemplate.from_template("""
-    This is an example of how you talk:
-
-    Human: {example_question}
-    You: {example_answer}
-""")
-
-start = PromptTemplate.from_template("""
-    Start now!
-
-    Human: {question}
-    You:
-""")
-
-final = PromptTemplate.from_template("""
-    {intro}
-
-    {example}
-
-    {start}
-""")
-
-prompts = [
-    ("intro", intro),
-    ("example", example),
-    ("start", start),
-]
-
-full_prompt = PipelinePromptTemplate(
-    final_prompt=final,
-    pipeline_prompts=prompts,
+chat = ChatOpenAI(
+    temperature=0.1,
+    # streaming=True,
+    # callbacks=[
+    #     StreamingStdOutCallbackHandler(),
+    # ],
 )
 
-chain = full_prompt | chat
+chat.predict("How do you make italian pasta") # 13초
 
-chain.invoke({
-    "character": "Pirate",
-    "example_question": "What is your location?",
-    "example_answer": "Arrrrg! That is a secret!! Arg arg!!",
-    "question": "What is your fav food?",
-})
+chat.predict("How do you make italian pasta") # 0초
