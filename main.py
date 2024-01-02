964faf38a84ca04fb68b5e789435931f82eedbb1
junho-baek <86907815+junho-baek@users.noreply.github.com>
2024-01-02 14:29:29 +0000

    #4.6 Serialization (05:23)
    사용량을 파악하는 방법과,
    chat 설정을 저장하는 법
    chat 설정을 불러오는 법을 배웠다.

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 from langchain.chat_models import ChatOpenAI
-from langchain.callbacks import StreamingStdOutCallbackHandler
-from langchain.globals import set_llm_cache, set_debug
-from langchain.cache import InMemoryCache, SQLiteCache
-
-set_llm_cache(SQLiteCache("cache.db"))
-
+from langchain.callbacks import get_openai_callback
 
 chat = ChatOpenAI(
     temperature=0.1,
-    # streaming=True,
-    # callbacks=[
-    #     StreamingStdOutCallbackHandler(),
-    # ],
 )
 
-chat.predict("How do you make italian pasta") # 13초
 
-chat.predict("How do you make italian pasta") # 0초
+with get_openai_callback() as usage:
+    a = chat.predict("What is the recipe for soju")
+    b = chat.predict("What is the recipe for bread")
+    print(a, "\n")
+    print(b, "\n")
+    print(usage)
+
+
+#사용량에 대해서 알 수 있다.
+
+from langchain.chat_models import ChatOpenAI
+from langchain.llms.openai import OpenAI
+
+chat = OpenAI(
+  temperature = 0.1,
+  max_tokens = 450,
+  model = "gpt-3.5-turbo-16k"
+)
+
+chat.save("model.json") # chat 설정을 저장.
+
+
+from langchain.llms.loading import load_llm
+
+chat = load_llm("model.json") # 모델을 불러올 수도 있다.
