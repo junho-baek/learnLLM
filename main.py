e044c95fbbdab2d98ff17fed3f2308e227c35d66
junho-baek <86907815+junho-baek@users.noreply.github.com>
2024-01-07 14:14:32 +0000

    #5.6 Chat Based Memory (04:03)
    chat 기반 메모리 남기는 법을 배웠다.

--- main.py
+++ main.py
@@ -4,26 +4,27 @@ from langchain.chat_models import ChatOpenAI
 
 from langchain.chains import LLMChain
 
-from langchain.prompts import PromptTemplate
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 
 llm = ChatOpenAI (temperature=0.1)
 memory = ConversationSummaryBufferMemory(
   llm=llm,
   max_token_limit=120,
   memory_key="chat_history",
+  return_messages=True,
 )
 
-template = """
-    You are a helpful AI talking to a human.
-
-    {chat_history}
-    Human:{question}
-    You:
-"""
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful AI talking to a human"),
+        MessagesPlaceholder(variable_name="chat_history"),#채팅 기록을 남긴다.
+        ("human", "{question}"),
+    ]
+)
 chain = LLMChain(
   llm=llm,
   memory=memory,
-  prompt= PromptTemplate.from_template(template),
+  prompt= prompt,
   verbose=True #프롬프트 로그들을 확인할 수 있는 프로퍼티
 )
 print(chain.predict(question="What is the capital of France?"))
