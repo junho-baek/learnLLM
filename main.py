104ee75ea6b06388dab60cc084b3ad4c08bf9292
junho-baek <86907815+junho-baek@users.noreply.github.com>
2024-01-07 04:34:54 +0000

    #5.4 ConversationKGMemory (03:52)
    # knowledge graph
    # 중요도에 따라 요약해준다
    from langchain.memory import ConversationKGMemory

--- main.py
+++ main.py
@@ -1,28 +1,24 @@
-
-# 메모리에n설정한 메세지의 수 넘어가면 요약해서 저장
-# 최근 대화에 더 중점을 두고 있는 요약 모델
-from langchain.memory import ConversationSummaryBufferMemory
+# knowledge graph
+# 중요도에 따라 요약해준다
+from langchain.memory import ConversationKGMemory
 from langchain.chat_models import ChatOpenAI
 
 llm = ChatOpenAI(temperature=0.1)
 
 
-memory = ConversationSummaryBufferMemory(
+memory = ConversationKGMemory(
   llm=llm,
-  max_token_limit = 150,
   return_messages=True
 )
 
 def add_message(input, output):
   memory.save_context({"input": input}, {"output": output})
 
-def get_history():
-  return memory.load_memory_variables({})
 
 
-add_message(input="안녕 나는 준호고 남한에 살고 있어", output="와 그거 멋진데?")
+# 저장
+add_message("say hi to sam", "who is sam")
+add_message("sam is a friend", "okay")
 
-
-add_message(input="남한은 매우 멋져", output="나도 갈 수 있으면 좋겠다..")
-
-print(get_history())
+# 출력
+print(memory.load_memory_variables({"input": "who is sam"}))
