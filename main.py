865cffc2403d0c5774f6a0879720f921b891475d
junho-baek <86907815+junho-baek@users.noreply.github.com>
2024-01-13 14:36:28 +0000

    #6.2 Tiktoken (04:19)
    오픈에이아이에서 만든 tictoken은 문자열을 AI가 학습하기 쉽게 토큰화하고, 그 토큰들은 고유의 숫자가 있다. 그래서 인공지능은 그 토큰들 간의 관계를 통계적으로 예측하지.

--- main.py
+++ main.py
@@ -1,26 +1,16 @@
 from langchain.chat_models import ChatOpenAI
-# from langchain.document_loaders import TextLoader
-# loader = TextLoader('./files/chapter_one.txt')
-# from langchain.document_loaders import PyPDFLoader
-# loader = PyPDFLoader('./files/sample.pdf')
-# 각각의 파일 로더가 아니라 구조화되어 있지 않은 파일을 불러오는 모듈
 from langchain.document_loaders import UnstructuredFileLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter #일반적인 방법의 텍스트 분할 모듈
-from langchain.text_splitter import CharacterTextSplitter #문자 단위로 분할하는 모듈
+from langchain.text_splitter import CharacterTextSplitter
 
-# splitter = RecursiveCharacterTextSplitter(
-#   chunk_size = 200,
-#   chunk_overlap = 50 #문장을 다 잘라먹을 경우에 앞 조각의 끝부분을 조금 가져와서 이어붙임.
-# )
-
-splitter = CharacterTextSplitter(
+splitter = CharacterTextSplitter.from_tiktoken_encoder(
   separator="\n",
   chunk_size = 600,
   chunk_overlap = 100,
-  # length_function = len
+  # length_function = len #얼마나 많은 문자를 사용하는지 len 함수를 이용해서 알려줌.
 )
+# 토크나이져로 모델이 진짜로 보는 토큰 단위를 끝어서 볼 수 있다. 토큰이란 문자를 단어조차 아닌 특정 접두 접미를 숫자를 부여해서 인공지능의 예측할 수 있는 학습데이터의 원형이라고 볼 수 있다.
 
-# loader = UnstructuredFileLoader('./files/sample.pdf')
+# ticktoken 은 오픈에이아이에서 만든거다.
 
 loader = UnstructuredFileLoader('./files/demo.docx')
 
