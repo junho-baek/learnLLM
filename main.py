544d6de51c086e66974b969127a672ca5452ae19
junho-baek <86907815+junho-baek@users.noreply.github.com>
2024-01-14 14:20:24 +0000

    #6.6 RetrievalQA (18:22)
     RetrievalQA  사용법에 대해 다뤘다.

--- main.py
+++ main.py
@@ -1,50 +1,37 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import UnstructuredFileLoader
 from langchain.text_splitter import CharacterTextSplitter
-# 벡터단위로 임베딩하는 모듈
 from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
-#벡터를 저장하는 공간
-from langchain.vectorstores import Chroma
+from langchain.vectorstores import FAISS
 from langchain.storage import LocalFileStore
+
+
+# 이건 Retrieval을 쉽게 할 수 있는 모듈인데, 다음시간에 프롬프트 엔지니어링을 통해서 이를 직접 구현할 것임.
+from langchain.chains import RetrievalQA
+
+llm = ChatOpenAI()
 
 cache_dir = LocalFileStore("./cache/")
 
-
-# embedder = OpenAIEmbeddings()
-
-# 단어 하나
-# vector = embedder.embed_query("Hi")
-# print(vector)
-
-# rank = len(vector)
-
-# print(rank)
-
-# 단어 여러개
-# words = ["Hi", "Hello", "Bye", "Goodbye", "Thank you", "Thanks", "You're welcome", "Yes", "No", "Sorry"]
-# vectors = embedder.embed_documents(words)
-
-# print(vectors)
-
 splitter = CharacterTextSplitter.from_tiktoken_encoder(
-  separator="\n",
-  chunk_size=600,
-  chunk_overlap=100
+    separator="\n",
+    chunk_size=600,
+    chunk_overlap=100,
 )
 loader = UnstructuredFileLoader("./files/chapter_one.txt")
+
 docs = loader.load_and_split(text_splitter=splitter)
 
 embeddings = OpenAIEmbeddings()
 
-cached_embeddings = CacheBackedEmbeddings.from_bytes_store(
-  embeddings,
-  cache_dir
+cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
+
+vectorstore = FAISS.from_documents(docs, cached_embeddings)
+
+chain = RetrievalQA.from_chain_type(
+    llm=llm,
+    chain_type="map_reduce", #stuff 가 디폴트값이다. refine, map_reduce, map_rerank, map_reduce_refine 등의 옵션이 있다. 이것이 궁금하면 공식 문서로!
+    retriever=vectorstore.as_retriever(), # retriever를 이용하면 vectorstore에서뿐만 아니라 데이터베이스와 같은 저장소에서도 document를 불러와 답변에 사용할 수 있다.
 )
 
-
-vectorstore = Chroma.from_documents(documents=docs, embedding=cached_embeddings)
-
-
-result = vectorstore.similarity_search("Where does winston live")
-
-#6.5 Langsmith (01:48)
+print(chain.run("Describe Victory Mansions"))
