68ebcf8c8b62d32bc188f1a1e66383eea6a0d72c
junho-baek <86907815+junho-baek@users.noreply.github.com>
2024-01-18 15:00:15 +0000

    #6.8 Stuff LCEL Chain (10:13)
    retriver: Stuff를 만들어 보았다.

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.storage import LocalFileStore
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema.runnable import RunnablePassthrough
 
-
-# 이건 Retrieval을 쉽게 할 수 있는 모듈인데, 다음시간에 프롬프트 엔지니어링을 통해서 이를 직접 구현할 것임.
-from langchain.chains import RetrievalQA
-
-llm = ChatOpenAI()
+llm = ChatOpenAI(
+    temperature=0.1,
+)
 
 cache_dir = LocalFileStore("./cache/")
 
@@ -28,13 +28,22 @@ cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir
 
 vectorstore = FAISS.from_documents(docs, cached_embeddings)
 
-chain = RetrievalQA.from_chain_type(
-    llm=llm,
-    chain_type="map_reduce", #stuff 가 디폴트값이다. refine, map_reduce, map_rerank, map_reduce_refine 등의 옵션이 있다. 이것이 궁금하면 공식 문서로!
-    retriever=vectorstore.as_retriever(), # retriever를 이용하면 vectorstore에서뿐만 아니라 데이터베이스와 같은 저장소에서도 document를 불러와 답변에 사용할 수 있다.
+retriver = vectorstore.as_retriever()
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a helpful assistant. Answer questions using only the following context. If you don't know the answer just say you don't know, don't make it up:\n\n{context}",
+        ),
+        ("human", "{question}"),
+    ]
 )
 
-print(chain.run("Describe Victory Mansions"))
+#RunnablePassthrough chain 이 innvoke 될 때 파라미터를 그대로 가져옴
+chain = {"context": retriver, "question": RunnablePassthrough()
+        } | prompt | llm
+
+print(chain.invoke("Describe Victory Mansions"))
 
 
-# load, split, embed(vector), cahce vector, vectorstore, chain(retrivalQA) 등을 배움
